31437b7c13b81a37fbc98afe7c7098dd15040992
marbokeeffe <mark.okeeffe@gmail.com>
2026-03-24 14:52:04 +0000

    Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { Suspense } from 'react'
 import AnalyticsListener from "./components/AnalyticsListener";
 
+const GA_MEASUREMENT_ID = 'G-5X0ZT7YP60'
+
 export const metadata: Metadata = {
   title: "Mark O'Keeffe Portraits",
   description: "Pencil portraiture by Mark O'Keeffe.",
@@ -15,9 +17,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <script 
+          async 
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_MEASUREMENT_ID}');
+            `,
+          }}
+        />
+      </head>
       <body>
         {children}
-        {/* Google Analytics */}
+        {/* Google Analytics Listener */}
         <Suspense fallback={null}>
           <AnalyticsListener />
         </Suspense>
